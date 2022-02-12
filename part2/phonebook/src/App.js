b3b4bbaa1cd7244801e6a876b8beadf03bd6ee34
Andrea C <mirthis@gmail.com>
2022-02-12 19:45:42 +0000

    complete 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,27 @@
 import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "111-111-111" },
+  ]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
 
   const submitForm = (e) => {
     e.preventDefault();
     if (persons.find((i) => i.name === newName))
       return alert(`${newName} is already added to phonebook`);
-    setPersons(persons.concat({ name: newName }));
+    setPersons(persons.concat({ name: newName, number: newNumber }));
     setNewName("");
+    setNewNumber("");
   };
 
   const onNameChange = (e) => {
     setNewName(e.target.value);
+  };
+
+  const onNumberChange = (e) => {
+    setNewNumber(e.target.value);
   };
 
   return (
@@ -23,6 +31,9 @@ const App = () => {
       <form onSubmit={submitForm}>
         <div>
           name: <input value={newName} onChange={onNameChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={onNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -36,6 +47,10 @@ const App = () => {
   );
 };
 
-const Person = ({ person }) => <div>{person.name}</div>;
+const Person = ({ person }) => (
+  <div>
+    {person.name} {person.number}
+  </div>
+);
 
 export default App;
