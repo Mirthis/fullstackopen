768944a484d8e237dfbfa7588f814243334e06eb
Andrea C <mirthis@gmail.com>
2022-02-12 19:58:11 +0000

    complete 2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,16 +2,25 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "111-111-111" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
 
   const submitForm = (e) => {
     e.preventDefault();
     if (persons.find((i) => i.name === newName))
       return alert(`${newName} is already added to phonebook`);
-    setPersons(persons.concat({ name: newName, number: newNumber }));
+    const newPerson = {
+      name: newName,
+      number: newNumber,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(newPerson));
     setNewName("");
     setNewNumber("");
   };
@@ -24,10 +33,25 @@ const App = () => {
     setNewNumber(e.target.value);
   };
 
+  const onFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+
+  const showPersons = ""
+    ? persons
+    : persons.filter((p) =>
+        p.name.toLowerCase().includes(filter.toLowerCase())
+      );
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>debug: {newName}</div>
+      <form onSubmit={submitForm}>
+        <div>
+          filter shown with: <input value={filter} onChange={onFilterChange} />
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={submitForm}>
         <div>
           name: <input value={newName} onChange={onNameChange} />
@@ -40,8 +64,8 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => (
-        <Person key={person.name} person={person} />
+      {showPersons.map((person) => (
+        <Person key={person.id} person={person} />
       ))}
     </div>
   );
