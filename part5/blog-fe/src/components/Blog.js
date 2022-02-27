5a71ba3b6e973515e15280aa2484b93f80473ce5
Andrea C <mirthis@gmail.com>
2022-02-27 18:46:56 +0000

    add view/hide for blogs (5.7)

--- part5/blog-fe/src/components/Blog.js
+++ part5/blog-fe/src/components/Blog.js
@@ -1,7 +1,41 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+import React from 'react'
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const blogDetails = () => (
+    <>
+      <div>{blog.url}</div>
+      <div>
+        likes: {blog.likes}
+        <button>like</button>
+      </div>
+    </>
+  )
+
+  const toggleVisibility = () => {
+    console.log(typeof setVisible)
+    setVisible(!visible)
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      </div>
+      {visible && blogDetails()}
+    </div>
+  )
+}
 
 export default Blog
