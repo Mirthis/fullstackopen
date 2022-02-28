0bd8f9848d79061e0ee77fac128beefbdecf0e74
Andrea C <mirthis@gmail.com>
2022-02-28 11:46:03 +0000

    add like button testing (5.15)

--- part5/blog-fe/src/components/Blog.test.js
+++ part5/blog-fe/src/components/Blog.test.js
@@ -7,6 +7,8 @@ import Blog from './Blog'
 describe('<Blog />', () => {
   let blog
   let container
+  let mockDeleteHandler
+  let mockLikeHandler
 
   beforeEach(() => {
     blog = {
@@ -17,14 +19,15 @@ describe('<Blog />', () => {
       likes: 10,
     }
 
-    const mockHandler = jest.fn()
+    mockLikeHandler = jest.fn()
+    mockDeleteHandler = jest.fn()
 
     container = render(
       <Blog
         blog={blog}
-        likeHandler={mockHandler}
+        likeHandler={mockLikeHandler}
         canBeDeleted={false}
-        deleteHandler={mockHandler}
+        deleteHandler={mockDeleteHandler}
       />
     ).container
   })
@@ -43,5 +46,32 @@ describe('<Blog />', () => {
 
     const likesDiv = container.querySelector('.blog-likes')
     expect(likesDiv).toHaveTextContent(`likes: ${blog.likes}`)
+  })
+
+  test('renders content', () => {
+    const div = container.querySelector('.blog')
+    expect(div).toHaveTextContent(`${blog.title} ${blog.author}`)
+  })
+
+  test('show details when show button is clicked', () => {
+    const button = screen.getByText('view')
+    userEvent.click(button)
+
+    const urlDiv = container.querySelector('.blog-url')
+    expect(urlDiv).toHaveTextContent(blog.url)
+
+    const likesDiv = container.querySelector('.blog-likes')
+    expect(likesDiv).toHaveTextContent(`likes: ${blog.likes}`)
+  })
+
+  test('call like handler when like button is clicked', () => {
+    const viewButton = screen.getByText('view')
+    userEvent.click(viewButton)
+
+    const likeButton = screen.getByText('like')
+    userEvent.click(likeButton)
+    expect(mockLikeHandler.mock.calls).toHaveLength(1)
+    userEvent.click(likeButton)
+    expect(mockLikeHandler.mock.calls).toHaveLength(2)
   })
 })
