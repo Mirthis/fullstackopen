b81040b1b5e1efae735a41c6682f0cdde3c03df2
Andrea C <mirthis@gmail.com>
2022-03-10 11:24:37 +0000

    populate books view (8.8)

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,9 +1,16 @@
-const Books = (props) => {
+import { ALL_BOOKS } from '../queries'
+import { useQuery } from '@apollo/client'
+
+const Books = props => {
   if (!props.show) {
     return null
   }
 
-  const books = []
+  const result = useQuery(ALL_BOOKS)
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+  const books = result.data.allBooks
 
   return (
     <div>
@@ -16,7 +23,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {books.map(a => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author}</td>
