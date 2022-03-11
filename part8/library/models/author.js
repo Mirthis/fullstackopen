24437c0f7c09539f2b63789575d6365740d3d187
Andrea C <mirthis@gmail.com>
2022-03-11 12:07:24 +0000

    Add custom validation messages (8.15)

--- part8/library/models/author.js
+++ part8/library/models/author.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    unique: true,
-    minlength: 4,
+    required: [true, 'Author name is mandatory'],
+    unique: [true, 'Author name must be unique'],
+    minlength: [4, 'Author name must be at least 2 characters long.'],
   },
   born: {
     type: Number,
--- part8/library/models/book.js
+++ part8/library/models/book.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const schema = new mongoose.Schema({
   title: {
     type: String,
-    required: true,
-    unique: true,
-    minlength: 2,
+    required: [true, 'Book title is mandatory'],
+    unique: [true, 'Book title must be unique'],
+    minlength: [2, 'Book title must be at least 2 characters long.'],
   },
   published: {
     type: Number,
