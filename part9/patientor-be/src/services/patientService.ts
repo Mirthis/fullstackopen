d684048fe46e7c46632925f989ae5ef5431cc64d
Andrea C <mirthis@gmail.com>
2022-03-16 11:13:43 +0000

    implement POST endpoint /api/patients (9.12)

--- part9/patientor-be/src/services/patientService.ts
+++ part9/patientor-be/src/services/patientService.ts
@@ -1,5 +1,6 @@
-import { NonSensitivePatient } from '../types';
+import { NonSensitivePatient, Patient, NewPatient } from '../types';
 import patients from '../../data/patient';
+import { v4 as uuid } from 'uuid';
 
 const getNonSensitiveEntries = (): NonSensitivePatient[] => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation }) => {
@@ -13,4 +14,15 @@ const getNonSensitiveEntries = (): NonSensitivePatient[] => {
   });
 };
 
-export default { getNonSensitiveEntries };
+const addPatient = (patient: NewPatient): Patient => {
+  const id: string = uuid();
+  const newPatient = {
+    id: id,
+    ...patient,
+  };
+
+  patients.push(newPatient);
+  return newPatient;
+};
+
+export default { getNonSensitiveEntries, addPatient };
---
+++ part9/patientor-be/src/utils.ts
@@ -0,0 +1,64 @@
+import { Gender, NewPatient } from './types';
+
+type Fields = {
+  name: unknown;
+  dateOfBirth: unknown;
+  ssn: unknown;
+  gender: unknown;
+  occupation: unknown;
+};
+
+const isString = (field: unknown): field is string => {
+  return typeof field === 'string' || field instanceof String;
+};
+
+const parseStringField = (field: unknown, fieldName: string): string => {
+  if (!field || !isString(field)) {
+    throw new Error(
+      `Incorrect or missing value for field ${fieldName}: ${field}`
+    );
+  }
+
+  return field;
+};
+
+const isDate = (date: string): boolean => {
+  return Boolean(Date.parse(date));
+};
+
+const parseDate = (date: unknown): string => {
+  if (!date || !isString(date) || !isDate(date)) {
+    throw new Error('Incorrect or missing date: ' + date);
+  }
+  return date;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const isGender = (param: any): param is Gender => {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+  return Object.values(Gender).includes(param);
+};
+
+const parseGender = (gender: unknown): Gender => {
+  if (!gender || !isGender(gender)) {
+    throw new Error('Incorrect or missing gender: ' + gender);
+  }
+  return gender;
+};
+
+export const toNewPatientEntry = ({
+  name,
+  dateOfBirth,
+  ssn,
+  gender,
+  occupation,
+}: Fields): NewPatient => {
+  const newPatient = {
+    name: parseStringField(name, 'name'),
+    dateOfBirth: parseDate(dateOfBirth),
+    ssn: parseStringField(ssn, 'ssn'),
+    gender: parseGender(gender),
+    occupation: parseStringField(occupation, 'occupation'),
+  };
+  return newPatient;
+};
